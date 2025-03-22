54b2e40cd53aa2da550fb20dacb81bbabbeba233
ananysagar <sagaranany577@gmail.com>
2025-03-23 00:24:37 +0530

    wrapping app inside the redux provider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import "./App.css";
+import { decrement, increment, reset } from "./redux/CounterSlice";
 
 function App() {
+  const counter = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      
+      <div className="counter">
+        <button id="decrement" onClick={() => dispatch(increment())}>+</button>
+        <div className="count">{counter}</div>
+        <button id="increment" onClick={() => dispatch(decrement())}>-</button>
+        <button id="reset" onClick={() => dispatch(reset())}>Reset</button>
+      </div>
     </div>
   );
 }
